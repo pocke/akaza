821b1984e9aedf14c6945a25639c32f2319f7563
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-05-01 00:02:07 +0900

    Fizz buzz test

--- test/akaza/ruby2ws_test.rb
+++ test/akaza/ruby2ws_test.rb
@@ -193,6 +193,42 @@ class Ruby2wsTest < Minitest::Test
     RUBY
   end
 
+  def test_transpile_fizzbuzz
+    assert_eval "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz ", <<~RUBY, StringIO.new("15\n")
+      def fizz
+        put_as_char 'f'
+        put_as_char 'i'
+        put_as_char 'z'
+        put_as_char 'z'
+      end
+
+      def buzz
+        put_as_char 'b'
+        put_as_char 'u'
+        put_as_char 'z'
+        put_as_char 'z'
+      end
+
+      max = get_as_number
+      n = 0 - max
+      while n < 0
+        x = max + n + 1
+        if x % 15 == 0
+          fizz
+          buzz
+        elsif x % 3 == 0
+          fizz
+        elsif x % 5 == 0
+          buzz
+        else
+          put_as_number x
+        end
+        put_as_char ' '
+        n = n + 1
+      end
+    RUBY
+  end
+
   class AnyClass
     def initialize(klass)
       @klass = klass
